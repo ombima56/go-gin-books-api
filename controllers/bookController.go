b467eea083806ffb49de740c04e3897c283f8a5c
ombima56 <ombimahillary6@gmail.com>
2024-07-16 03:53:49 +0300

    feat: removes slice struct for models.Books and add functions for models.DB

--- controllers/bookController.go
+++ controllers/bookController.go
@@ -1,43 +1,28 @@
 package controllers
 
 import (
-	"net/http"
-	"strconv"
-
 	"go-gin-books-api/models"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-var books = []models.Book{
-	{ID: 1, Title: "Pride and Prejudice", Author: "Jane Austine"},
-	{ID: 2, Title: "The Ransom Trilogy", Author: "C.S.Lewis"},
-	{ID: 3, Title: "To Kill a Mockingbird", Author: "Harper Lee"},
-	{ID: 4, Title: "1984", Author: "George Orwell"},
-	{ID: 5, Title: "The Catcher in the Rye", Author: "J.D. Salinger"},
-	{ID: 6, Title: "Harvard Address", Author: "Alexander Solzhenitsyn"},
-}
-
 func GetBooks(c *gin.Context) {
+	var books []models.Book
+	models.DB.Find(&books)
 	c.IndentedJSON(http.StatusOK, books)
 }
 
 func GetBookByID(c *gin.Context) {
 	id := c.Param("id")
+	var book models.Book
 
-	bookId, err := strconv.ParseInt(id, 10, 64)
-	if err != nil {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Invalid book ID"})
+	if err := models.DB.First(&book, id).Error; err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
 		return
 	}
 
-	for _, book := range books {
-		if book.ID == uint(bookId) {
-			c.IndentedJSON(http.StatusOK, book)
-			return
-		}
-	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+	c.IndentedJSON(http.StatusOK, book)
 }
 
 func CreateBook(c *gin.Context) {
@@ -48,49 +33,37 @@ func CreateBook(c *gin.Context) {
 		return
 	}
 
-	newBook.ID = uint(len(books) + 1)
-	books = append(books, newBook)
+	models.DB.Create(&newBook)
 	c.IndentedJSON(http.StatusCreated, newBook)
 }
 
 func UpdateBook(c *gin.Context) {
 	id := c.Param("id")
+	var book models.Book
 
-	bookID, err := strconv.Atoi(id)
-	if err != nil {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Invalid book ID"})
+	if err := models.DB.First(&book, id).Error; err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
 		return
 	}
 
-	var updateBook models.Book
-	if err := c.BindJSON(&updateBook); err != nil {
+	if err := c.BindJSON(&book); err != nil {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Invalid request body"})
 		return
 	}
 
-	for i, book := range books {
-		if book.ID == uint(bookID) {
-			books[i] = updateBook
-			c.IndentedJSON(http.StatusOK, updateBook)
-		}
-	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+	models.DB.Save(&book)
+	c.IndentedJSON(http.StatusOK, book)
 }
 
 func DeleteBook(c *gin.Context) {
 	id := c.Param("id")
-	bookID, err := strconv.Atoi(id)
-	if err != nil {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Invalid Book ID"})
+	var book models.Book
+
+	if err := models.DB.First(&book, id).Error; err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
 		return
 	}
 
-	for i, book := range books {
-		if book.ID == uint(bookID) {
-			books = append(books[:i], books[i+1:]...)
-			c.IndentedJSON(http.StatusOK, gin.H{"messsage": "Book deleted"})
-			return
-		}
-	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+	models.DB.Delete(&book)
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "Book deleted"})
 }
