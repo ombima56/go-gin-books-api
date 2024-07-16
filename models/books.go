37875ac3ae002d3258cc63cba4a4cd736a73ed34
ombima56 <ombimahillary6@gmail.com>
2024-07-16 03:58:22 +0300

    feat: add function ConnectDataBase

--- models/books.go
+++ models/books.go
@@ -1,7 +1,27 @@
 package models
 
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
 type Book struct {
-	ID     uint   `json:"id"`
+	ID     uint   `gorm:"primaryKey" json:"id"`
 	Title  string `json:"title"`
 	Author string `json:"author"`
+}
+
+var DB *gorm.DB
+
+func ConnectDataBase() {
+	dsn := "host=localhost user=ombima password=5897 dbname=go_gin_books_api port=5432 sslmode=disable TimeZone=Africa/Nairobi"
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Println("Failed to connect to database!", err)
+	}
+	database.AutoMigrate(&Book{})
+
+	DB = database
 }
